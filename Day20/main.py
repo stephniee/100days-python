99461925eb8af61de59b6f111b6a0080eccd8211
2212072 <2212072@slu.edu.ph>
2025-04-12 19:16:22 +0800

    added assets

--- Day20/main.py
+++ Day20/main.py
@@ -5,30 +5,35 @@ from Snake import Snake
 # Screensetup
 screen = Screen()
 screen.setup(width=600,height=600)
-screen.bgcolor("#8ab565")
+screen.bgpic("Day20/assets/nokia.png")
 screen.title("My Snake Game")
 screen.tracer(0)
-
-
-
-# Call Snake
-snake_body = Snake()
-game_is_on = True
-
-# Keystroke Listener
 screen.listen()
-screen.onkey(snake_body.up,"w")
-screen.onkey(snake_body.down,"s")
-screen.onkey(snake_body.left,"a")
-screen.onkey(snake_body.right,"d")
+# def tap_to_change():
+#     screen.bgcolor("#8ab565")
+ 
+  
+# Keystroke Listener
 
+game_is_on = True
+# if screen.onkey(tap_to_change,"space"):
+#     game_is_on = True
 
+# else:
+#     game_is_on = False
 
 while game_is_on:
     screen.update()
-    
+    snake_body = Snake()
+ 
     time.sleep(.1)
-    
+    #Call Snake
+
+   
+    screen.onkey(snake_body.up,"Up")
+    screen.onkey(snake_body.down,"Down")
+    screen.onkey(snake_body.left,"Left")
+    screen.onkey(snake_body.right,"Right")
     snake_body.follow()
  
         
